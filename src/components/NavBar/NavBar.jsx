f03152c291577fff59bcfa26c28987e395f9c0b2
Moin Khan <kmoin786@gmail.com>
2021-03-15 14:59:48 +0530

    Style menu components based on specifications

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,36 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 import HnLogo from '../../images/hn-logo.png';
 import './NavBar.styles.css';
 
 const NavBar = ({ primary, loginStatus, colorScheme }) => (
-  <nav>
-    <img src={HnLogo} alt="Hacker Noon logo" />
+  <div className={`h-menu color-${colorScheme}`}>
+    <a href="./" className="logo-link">
+      <img className="hn-logo" src={HnLogo} alt="Hacker Noon logo" />
+    </a>
     <div className={`nav-right ${primary ? 'show' : 'hide'}`}>
-      <form id="main-nav-search">
-        <label htmlFor="search">Search</label>
-        <input type="search" name="search" id="search" />
-        <button type="submit">Search</button>
+      <form id="main-nav-search" className="search-form">
+        <label className="label-name hide" htmlFor="search">Search</label>
+        <input type="search" name="search" placeholder="Search..." className="search-input" id="search" />
+        <button className="search-submit" type="submit"><span>Search</span></button>
       </form>
-      <nav>
-        <ul className={`color-${colorScheme}`}>
-          <li>Start Writing</li>
-          {loginStatus ? <li className="menu-profile-pic" /> : <li>Log in</li>}
-          <li className={colorScheme === 'light' ? 'menu-moon' : 'menu-sun'} />
+      <nav className="nav-right-menu-container">
+        <ul className={`nav-right-menu color-${colorScheme}`}>
+          <li className="menu-item write-cta"><a href="./" className="menu-link">Start Writing</a></li>
+          <li className={`menu-item menu-profile ${loginStatus ? 'menu-profile-pic' : ''}`}>
+            {loginStatus ? '' : <a href="./" className="menu-link">Log in</a>}
+          </li>
+          <li className={`menu-item color-icon ${colorScheme === 'light' ? 'menu-moon' : 'menu-sun'}`} />
         </ul>
       </nav>
     </div>
-  </nav>
+  </div>
 );
+
+NavBar.propTypes = {
+  primary: PropTypes.bool.isRequired,
+  loginStatus: PropTypes.bool.isRequired,
+  colorScheme: PropTypes.string.isRequired,
+};
 
 export default NavBar;
