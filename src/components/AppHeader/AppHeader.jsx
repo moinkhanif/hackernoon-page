dc3cdb052b4af311aaa62d75e567316fbe66f9c0
Moin Khan <kmoin786@gmail.com>
2021-03-17 00:07:25 +0530

    Add sponsored bar and style the component and its children

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,10 +1,12 @@
 import PrimaryNavBar from '../PrimaryNavBar/PrimaryNavBar';
 import PrimarySubNavBar from '../PrimarySubNavBar/PrimarySubNavBar';
+import SponsorNavBar from '../SponsorNavBar/SponsorNavBar';
 
 const AppHeader = () => (
   <header className="App-header">
     <PrimaryNavBar />
     <PrimarySubNavBar />
+    <SponsorNavBar />
   </header>
 );
 
