bdb71943d4c5c942d21a56c1c962a1f31b8cb29e
Moin Khan <kmoin786@gmail.com>
2021-03-17 13:48:03 +0530

    Add post date to date-bookmark container

--- src/components/PostContent/PostContent.jsx
+++ src/components/PostContent/PostContent.jsx
@@ -1,16 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import postData from '../../data/postData.json';
 import './PostContent.styles.css';
 
 const PostContent = () => {
-  useEffect(() => {
+  const [publishedMonth, setPublishedMonth] = useState('');
+  const [publishedMonthName, setPublishedMonthName] = useState('');
+  const [publishedDate, setPublishedDate] = useState('');
+  const [publishedYear, setPublishedYear] = useState('');
 
+  const ordinal = number => {
+    if (number !== '') {
+      const englishOrdinalRules = new Intl.PluralRules('en', {
+        type: 'ordinal',
+      });
+      const suffixes = {
+        one: 'st',
+        two: 'nd',
+        few: 'rd',
+        other: 'th',
+      };
+      const suffix = suffixes[englishOrdinalRules.select(number)];
+      return (number + suffix);
+    }
+    return '';
+  };
+
+  useEffect(() => {
+    const postDate = new Date(postData.publishedAt * 1000);
+    setPublishedMonth(postDate.getMonth() + 1);
+    setPublishedDate(postDate.getDate());
+    setPublishedYear(postDate.getFullYear());
+    setPublishedMonthName(postDate.toLocaleString('default', { month: 'long' }));
   });
 
   return (
-    <div className="post-container">
+    <main className="post-container">
       <h1 className="post-title">{postData.title}</h1>
-    </div>
+      <div className="main-post-meta">
+        <div className="date-bookmark-container">
+          <a
+            className="post-date"
+            href={`/archives/${publishedYear}/${publishedMonth < 10 ? '0' : ''}${publishedMonth}/${publishedDate}`}
+          >
+            {`${publishedMonthName} ${ordinal(publishedDate)} ${publishedYear}`}
+          </a>
+        </div>
+      </div>
+    </main>
   );
 };
 
